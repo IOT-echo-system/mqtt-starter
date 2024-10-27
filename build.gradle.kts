410f8fc06e01323b38257e73f57f5f791e71652f
Shivam Rajput <raj.shiviraj@gmail.com>
2024-10-27 09:51:01 +0530

    Shivi | updated gradle version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ publishing {
     repositories {
         maven {
             name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/IOT-echo-system/logging-starter")
+            url = uri("https://maven.pkg.github.com/IOT-echo-system/mqtt-starter")
             credentials {
                 username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
                 password = project.findProperty("gpr.token") as String? ?: System.getenv("TOKEN")
